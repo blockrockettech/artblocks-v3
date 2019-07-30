e3375abcb812e2271ce90fc56ad1546e862afad4
James Morgan <james.morgan.e@googlemail.com>
2019-07-30 11:45:47 +0100

    Adding base URK

--- migrations/2_deploy_simpleartisttoken.js
+++ migrations/2_deploy_simpleartisttoken.js
@@ -6,5 +6,6 @@ module.exports = async function (deployer, network, accounts) {
 
     const artblocksAccount = getAccountOne(accounts, network);
 
+    // TODO this should have a better base URL?
     await deployer.deploy(SimpleArtistToken, artblocksAccount, 1, 'ipfs', 5, {from: artblocksAccount});
 };
